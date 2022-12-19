7e0310fcfb64b192b4f9ae3aaf2a55fe9b67b01b
MetadrainCoding <120743762+MetadrainCoding@users.noreply.github.com>
2022-12-19 07:09:41 -0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+const dataContent = {
+    name: "Pre-Sale Inferno", // Name
+    supply: "1200", // supply value
+    socialMedia: {
+        discord: "https://discord.gg/2StBGaAHgy", // if not, set empty
+        twitter: "https://twitter.com/entertheinferno", // if not, set empty
+    },
+    medias: {
+        nft: "./assets/preview.png", // nft image
+        background: "./assets/background.png", // background image
+        favicon: "./assets/icon.png", // favicon image
+    }
+}
+
+// Wallet, mint price settings in index.html
